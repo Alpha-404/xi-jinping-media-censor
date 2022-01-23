610399c91bcf555b178be5c77827a46c76b824ce
Syntax Error <63340001+Alpha-404@users.noreply.github.com>
2022-01-23 10:06:37 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-link = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.XNhppFZLTOVlQUSXtVgwvwHaHa%26pid%3DApi&f=1"
-loop = (function censor(i) {
-  setTimeout(function() {
+link = "https://external-content.duckduckgo.com/iu/?u=https://github.com/Alpha-404/xi-jinping-media-censor/blob/main/hd%20xi%20jinping.jpg?raw=true";
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+while (true) {
+    console.log("media censored haha");
     for(var i=0; i<document.images.length; i++) {
-    	if (document.images[i].src == link) {continue}
-		document.images[i].setAttribute("style", `height:${document.images[i].height}px; width:${document.images[i].width}px; object-fit:cover; content:url(${link});`);
-		document.images[i].src = link;
-	}              
-    if (--i) censor(i);
-  }, 500)
-})(10); 
+        if (document.images[i].src == link) {continue;}
+        document.images[i].setAttribute("style", `height:${document.images[i].height}px; width:${document.images[i].width}px; object-fit:cover; content:url(${link}); opacity:1; -moz-opacity:1; filter:alpha(opacity=100)`);
+        document.images[i].src = link;
+        document.images[i].style.display = "initial"; 
+    }
+    await sleep(500);
+}
